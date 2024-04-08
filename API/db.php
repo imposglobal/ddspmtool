d4d1e97e535023b1e2105d5eae3773afe8b4306e
imposglobal <rushikesh@imposglobal.com>
2024-04-08 15:30:17 +0530

    db

--- API/db.php
+++ API/db.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
 $host = 'localhost'; // or your database host
-$dbname = 'doodlueg_task';
-$username = 'root';
-$password = '';
+$dbname = 'ballapo7_pmtool';
+$username = 'ballapo7_pmtool';
+$password = 'pmtool@2024';
 
 // Create a MySQLi connection
 $db = new mysqli($host, $username, $password, $dbname);
