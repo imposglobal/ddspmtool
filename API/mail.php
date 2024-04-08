35d6a117ea2bb086309843361036463f83fc912d
imposglobal <rushikesh@imposglobal.com>
2024-04-08 16:15:05 +0530

    mail

--- API/mail.php
+++ API/mail.php
@@ -1,34 +1,45 @@
 <?php 
+require 'phpmailer/vendor/autoload.php'; 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 function welcomeEmail($email, $fname, $lname, $username, $password){
     $employeeName = $fname . " " . $lname;
-    $to = $email;
-    $subject = 'Welcome to Doodlo Designs Studio Project Management Tool!';
-    $message = "
-        <p>Dear $employeeName,</p>
-        <p>Welcome to Doodlo Designs Studio Project Management Tool! We are thrilled to have you join us in our endeavor to streamline our project management processes and enhance collaboration within our team.</p>
-        <p>Your account has been successfully created, and here are your login credentials:</p>
-        <p>User ID: $username</p>
-        <p>Password: $password</p>
-        <p>Access Link: <a href='http://dds.doodlodesign.com/'>http://dds.doodlodesign.com/</a></p>
-        <p>Please keep this information secure and do not share it with anyone. If you have any concerns regarding your account security or need assistance, feel free to reach out to our IT support team at <a href='mailto:rushikesh@imposglobal.com'>rushikesh@imposglobal.com</a>.</p>
-        <p>With Doodlo Designs Studio Project Management Tool, you'll have access to a range of features designed to simplify project planning, task management, communication, and more. We believe this tool will greatly facilitate our workflow and help us achieve our project goals efficiently.</p>
-        <p>To get started, simply log in using the provided credentials and explore the various functionalities available to you. We encourage you to familiarize yourself with the platform, and should you have any questions or require guidance, do not hesitate to contact our designated project management team or refer to the user guide provided.</p>
-        <p>Thank you for being a part of the Doodlo Designs Studio team. We look forward to working together and achieving great success on our projects.</p>
-        <p>Best regards,</p>
-        <p>Doodlo Designs Studio</p>
-    ";
-    
-    // Headers
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= "From: dds@doodlodesigns.com" . "\r\n";
+    $mail = new PHPMailer(true);
 
-    // Sending email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email has been sent";
-    } else {
-        echo "Message could not be sent.";
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'mail.doodlodesigns.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'dds@doodlodesigns.com';
+        $mail->Password = 'doodlo@2024';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        $mail->setFrom('dds@doodlodesigns.com', 'Doodlo Design Studio');
+        $mail->addAddress($email, $employeeName);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Welcome to Doodlo Designs Studio Project Management Tool!';
+        $mail->Body = "<p>Dear $employeeName,</p>
+            <p>Welcome to Doodlo Designs Studio Project Management Tool! We are thrilled to have you join us in our endeavor to streamline our project management processes and enhance collaboration within our team.</p>
+            <p>Your account has been successfully created, and here are your login credentials:</p>
+            <p>User ID: $username</p>
+            <p>Password: $password</p>
+            <p>Access Link: <a href='http://dds.doodlodesign.com/'>http://dds.doodlodesign.com/</a></p>
+            <p>Please keep this information secure and do not share it with anyone. If you have any concerns regarding your account security or need assistance, feel free to reach out to our IT support team at <a href='mailto:rushikesh@imposglobal.com'>rushikesh@imposglobal.com</a>.</p>
+            <p>With Doodlo Designs Studio Project Management Tool, you'll have access to a range of features designed to simplify project planning, task management, communication, and more. We believe this tool will greatly facilitate our workflow and help us achieve our project goals efficiently.</p>
+            <p>To get started, simply log in using the provided credentials and explore the various functionalities available to you. We encourage you to familiarize yourself with the platform, and should you have any questions or require guidance, do not hesitate to contact our designated project management team or refer to the user guide provided.</p>
+            <p>Thank you for being a part of the Doodlo Designs Studio team. We look forward to working together and achieving great success on our projects.</p>
+            <p>Best regards,</p>
+            <p>Doodlo Designs Studio</p>";
+
+        $mail->send();
+        echo 'Email has been sent';
+    } catch (Exception $e) {
+        // Log the error, if needed.
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 
