4617c30a31779c6144064073e8efe04d48642ec5
KrushnaPhad <164015177+KrushnaPhad@users.noreply.github.com>
2024-04-09 12:07:15 +0530

    new file opeartion.php created & added data in view_employee.php

---
+++ API/operation.php
@@ -0,0 +1,32 @@
+<?php
+require("db.php");
+
+// Function to view employees details
+function get_employees($db, $page = 1, $recordsPerPage = 10){
+   
+    $offset = ($page - 1) * $recordsPerPage;   // Calculate offset
+    $sql = "SELECT * FROM employees LIMIT $offset, $recordsPerPage";  // Fetch employees with pagination
+    $result = mysqli_query($db, $sql);
+    // print_r($result);
+    
+    if (mysqli_num_rows($result) > 0) {
+        // Output data of each row
+        $i = ($page - 1) * $recordsPerPage + 1;
+        while($row = mysqli_fetch_assoc($result)) {
+    
+            echo '<tr>';
+            echo '<th scope="row">'. $i++.'</th>';
+            echo '<td>'. $row["fname"]." ". $row["lname"].'</td>';
+            echo '<td>'. $row["email"].'</td>';
+            echo '<td>'. $row["designation"].'</td>';
+            echo '<td>'. $row["department"].'</td>';
+            echo '<td><a href="project.php?empid='. $row["emp_id"].'"><i class="bi bi-info-circle-fill"></i> View</td>';
+            echo '</tr>';
+        }
+    } else {
+        echo "<tr><td colspan='5'>No results found.</td></tr>";
+    }
+    
+}
+
+?>
