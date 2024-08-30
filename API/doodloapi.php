08db3c2fb619dda8e9b73f7150fb56e76e1ad79b
imposglobal <158268388+imposglobal@users.noreply.github.com>
2024-08-30 17:08:40 +0530

    json

--- API/doodloapi.php
+++ API/doodloapi.php
@@ -2,9 +2,16 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8"); // Set content type to JSON
+
+$response = [];
 
 if(isset($_POST['name'])){
   $name = $_POST['name'];
-  echo $name;
+  // Add data to the response array
+  $response['name'] = $name;
 }
- ?>
+
+// Encode the response array as JSON and output it
+echo json_encode($response);
+?>
