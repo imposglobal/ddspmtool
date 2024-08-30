377971ad29b20e05e45bb7b198fbad2b6efb31ed
imposglobal <158268388+imposglobal@users.noreply.github.com>
2024-08-30 16:53:45 +0530

    Update doodloapi.php

--- API/doodloapi.php
+++ API/doodloapi.php
@@ -1,1 +1,14 @@
-<?php echo"hii"; ?>
+<?php
+$allowedOrigins = [
+   'https://doodlodesigns.com',
+];
+ 
+if(in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins))
+{
+ $http_origin = $_SERVER['HTTP_ORIGIN'];
+} else {
+ $http_origin = "'https://doodlodesigns.com";
+}
+header("Access-Control-Allow-Origin: $http_origin");
+
+echo"hii"; ?>
