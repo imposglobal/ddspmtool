0429794648db30a5f6a1fed5fcb9ae76bea423b6
imposglobal <158268388+imposglobal@users.noreply.github.com>
2024-08-30 17:24:37 +0530

    Update doodloapi.php

--- API/doodloapi.php
+++ API/doodloapi.php
@@ -2,7 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-<?php
 header("Content-Type: application/json");
 
 $data = json_decode(file_get_contents("php://input"), true);
