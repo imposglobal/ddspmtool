8e6e4fcb8a05f361b5d77723a7ae3aadcda80f43
imposglobal <158268388+imposglobal@users.noreply.github.com>
2024-08-30 17:07:11 +0530

    name

--- API/doodloapi.php
+++ API/doodloapi.php
@@ -3,4 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-echo"hii"; ?>
+if(isset($_POST['name'])){
+  $name = $_POST['name'];
+  echo $name;
+}
+ ?>
