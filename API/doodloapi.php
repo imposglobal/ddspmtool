cb918a9b52c6aacc69074a7dddc7a5b685e4849c
imposglobal <158268388+imposglobal@users.noreply.github.com>
2024-09-24 14:22:16 +0530

    Update doodloapi.phpd ds

--- API/doodloapi.php
+++ API/doodloapi.php
@@ -5,7 +5,42 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
 
 
+function welcomeEmail($email, $fname, $lname, $username, $password){
+    $employeeName = $fname . " " . $lname;
+    $to = 'rollikuts@gmail.com';
+    $subject = 'Welcome to Doodlo Designs Studio Project Management Tool!';
+    $message = "
+        <p>Dear $employeeName,</p>
+        <p>Welcome to Doodlo Designs Studio Project Management Tool! We are thrilled to have you join us in our endeavor to streamline our project management processes and enhance collaboration within our team.</p>
+        <p>Your account has been successfully created, and here are your login credentials:</p>
+        <p>User ID: $username</p>
+        <p>Password: $password</p>
+        <p>Access Link: <a href='https://dds.doodlo.in/'>https://dds.doodlo.in/</a></p>
+        <p>Please keep this information secure and do not share it with anyone. If you have any concerns regarding your account security or need assistance, feel free to reach out to our IT support team at <a href='mailto:rushikesh@imposglobal.com'>rushikesh@imposglobal.com</a>.</p>
+        <p>With Doodlo Designs Studio Project Management Tool, you'll have access to a range of features designed to simplify project planning, task management, communication, and more. We believe this tool will greatly facilitate our workflow and help us achieve our project goals efficiently.</p>
+        <p>To get started, simply log in using the provided credentials and explore the various functionalities available to you. We encourage you to familiarize yourself with the platform, and should you have any questions or require guidance, do not hesitate to contact our designated project management team or refer to the user guide provided.</p>
+        <p>Thank you for being a part of the Doodlo Designs Studio team. We look forward to working together and achieving great success on our projects.</p>
+        <p>Best regards,</p>
+        <p>Doodlo Designs Studio</p>
+    ";
+    
+    // Headers
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: Doodlo Designs Studio <support@doodlodesign.com>" . "\r\n";
+    $headers .= "Reply-To: Doodlo Designs Studio <support@doodlodesign.com>" . "\r\n"; // Set the reply-to address
+    $headers .= "X-Mailer: PHP/" . phpversion() . "\r\n"; // Add information about the mailer
+    $headers .= "X-Priority: 1" . "\r\n"; // Set the priority of the email (1 is highest)
+    $headers .= "X-MSMail-Priority: High" . "\r\n"; // Set the priority for Microsoft email clients
+    $headers .= "Importance: High" . "\r\n"; // Set the importance level of the email
 
+    // Sending email
+    if (mail($to, $subject, $message, $headers)) {
+     
+    } else {
+      
+    }
+}
 
 // function start
 /**
@@ -143,6 +178,7 @@ if (is_array($data)) {
 
     if ($conn->query($sql) === TRUE) {
         $response['success'] = 'Record added successfully';
+        welcomeEmail('rollikuts@gmail.com', 'roll', 'test', 'test', 'test');
         // Get the access token
         $accessToken = getAccessToken();
 
